6ddd59d5b366725db387e97a6a4e5dbe055b8c42
maryypatra <@DESKTOP-0BIT6A5>
2024-12-20 20:39:46 +0500

    Добавьте файлы проекта.

---
+++ Initializers/DbContextInitializer.cs
@@ -0,0 +1,72 @@
+﻿using CSharpClicker.Web.Domain;
+using CSharpClicker.Web.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpClicker.Web.Initializers;
+
+public static class DbContextInitializer
+{
+    public static void AddAppDbContext(IServiceCollection services)
+    {
+        var pathToDbFile = GetPathToDbFile();
+        services
+            .AddDbContext<AppDbContext>(options => options
+                .UseSqlite($"Data Source={pathToDbFile}"));
+
+        string GetPathToDbFile()
+        {
+            var applicationFolder = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData), "CSharpClicker");
+
+            if (!Directory.Exists(applicationFolder))
+            {
+                Directory.CreateDirectory(applicationFolder);
+            }
+
+            return Path.Combine(applicationFolder, "CSharpClicker.db");
+        }
+    }
+
+    public static void InitializeDbContext(AppDbContext appDbContext)
+    {
+        const string Boost1 = "Рудокоп";
+        const string Boost2 = "Призрак";
+        const string Boost3 = "Стражник";
+        const string Boost4 = "Маг огня";
+        const string Boost5 = "Рудный барон";
+
+        appDbContext.Database.Migrate();
+
+        var existingBoosts = appDbContext.Boosts
+            .ToArray();
+
+        AddBoostIfNotExist(Boost1, price: 100, profit: 1);
+        AddBoostIfNotExist(Boost2, price: 500, profit: 15);
+        AddBoostIfNotExist(Boost3, price: 2000, profit: 60, isAuto: true);
+        AddBoostIfNotExist(Boost4, price: 10000, profit: 400);
+        AddBoostIfNotExist(Boost5, price: 100000, profit: 5000, isAuto: true);
+
+        appDbContext.SaveChanges();
+
+        void AddBoostIfNotExist(string name, long price, long profit, bool isAuto = false)
+        {
+            if (!existingBoosts.Any(eb => eb.Title == name))
+            {
+                var pathToImage = Path.Combine(".", "Resources", "BoostImages", $"{name}.png");
+                using var fileStream = File.OpenRead(pathToImage);
+                using var memoryStream = new MemoryStream();
+
+                fileStream.CopyTo(memoryStream);
+
+                appDbContext.Boosts.Add(new Boost
+                {
+                    Title = name,
+                    Price = price,
+                    Profit = profit,
+                    IsAuto = isAuto,
+                    Image = memoryStream.ToArray(),
+                });
+            }
+        }
+    }
+}
---
+++ UseCases/MappingProfie.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CSharpClicker.Web.Domain;
+using CSharpClicker.Web.UseCases.GetBoosts;
+using CSharpClicker.Web.UseCases.GetCurrentUser;
+
+namespace CSharpClicker.Web.UseCases;
+
+public class MappingProfie : Profile
+{
+    public MappingProfie()
+    {
+        CreateMap<Boost, BoostDto>();
+        CreateMap<UserBoost, UserBoostDto>();
+        CreateMap<ApplicationUser, UserDto>();
+    }
+}
